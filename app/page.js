1f05a11abd8e74b36c181897c0cc941af9db63e7
tschoerv <tschoerv@protonmail.com>
2024-05-19 12:35:35 +0200

    update for airdrop round 2

--- app/page.js
+++ app/page.js
@@ -4,13 +4,10 @@ import Image from "next/image";
 import Head from 'next/head';
 import { Button, Input, Link } from "@nextui-org/react";
 import '@rainbow-me/rainbowkit/styles.css';
-import { reconnect } from '@wagmi/core'
-import { injected } from '@wagmi/connectors'
-import { config } from './providers'
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { useWriteContract, useSwitchChain, useSimulateContract, useAccount, useReadContract } from "wagmi";
 
-import round1_whitelist from "./ABI/ROUND1-WHITELIST.json";
+import round2_whitelist from "./ABI/ROUND2-WHITELIST.json";
 import claim_contract_ABI from "./ABI/claim_contract_ABI.json";
 
 export default function Home() {
@@ -19,6 +16,7 @@ export default function Home() {
   const [userData, setUserData] = useState(null);
   const [manualAddress, setManualAddress] = useState("");
   const [manualCheck, setManualCheck] = useState(false);
+  const [checkTriggered, setCheckTriggered] = useState(false);
 
   const claim_contract_address = process.env.NEXT_PUBLIC_CLAIM_CONTRACT_ADDRESS;
 
@@ -32,7 +30,7 @@ export default function Home() {
 
   useEffect(() => {
     if (isConnected && address) {
-      const user = round1_whitelist.find(user => user.address.toLowerCase() === address.toLowerCase());
+      const user = round2_whitelist.find(user => user.address.toLowerCase() === address.toLowerCase());
       if (user) {
         setIsEligible(true);
         setUserData(user);
@@ -40,34 +38,37 @@ export default function Home() {
         setIsEligible(false);
         setUserData(null);
       }
+      setManualAddress(address);  // Preset the connected address in the input field
     }
   }, [address, isConnected]);
 
   const handleManualCheck = () => {
-    const user = round1_whitelist.find(user => user.address.toLowerCase() === manualAddress.toLowerCase());
+    const user = round2_whitelist.find(user => user.address.toLowerCase() === manualAddress.toLowerCase());
     if (user) {
       setIsEligible(true);
       setUserData(user);
-      setManualCheck(true);
     } else {
       setIsEligible(false);
       setUserData(null);
-      setManualCheck(true);
+      setHasClaimed(false);  // Reset the claimed status
     }
+    setManualCheck(true);
+    setCheckTriggered(true);
   };
 
   const { data: readHasClaimed, isSuccess: isSuccessReadHasClaimed } = useReadContract({
     address: claim_contract_address,
     abi: claim_contract_ABI,
     functionName: 'claimedByWallet',
-    args: [address]
+    args: [manualAddress],
   });
 
   useEffect(() => {
-    if (isSuccessReadHasClaimed && readHasClaimed !== undefined) {
-      setHasClaimed(parseInt(readHasClaimed) > 0);
+    if (checkTriggered && isConnected && isSuccessReadHasClaimed && readHasClaimed !== undefined) {
+      setHasClaimed(parseInt(readHasClaimed) > 1);
+      setCheckTriggered(false); // Reset the check trigger after the check is completed
     }
-  }, [readHasClaimed, isSuccessReadHasClaimed]);
+  }, [readHasClaimed, isSuccessReadHasClaimed, checkTriggered, isConnected]);
 
   const formatProof = (proof) => {
     if (!Array.isArray(proof)) return [];
@@ -78,14 +79,11 @@ export default function Home() {
     address: claim_contract_address,
     abi: claim_contract_ABI,
     functionName: 'claim',
-    args: userData ? [address, userData.amount, formatProof(userData.proof.split(','))] : undefined,
+    args: userData ? [manualAddress, userData.amount, formatProof(userData.proof.split(','))] : undefined,
     account: address,
   });
 
   const { writeContract: claimAirdrop } = useWriteContract();
-
-
-
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
@@ -101,7 +99,7 @@ export default function Home() {
           alt="bitcoin logo"
         />
       </Link>
-      <h1 className='text-3xl mt-4'>Airdrop Round 1</h1>
+      <h1 className='text-3xl mt-4'>Airdrop Round 2</h1>
       <div className="text-center mt-4">
         {chain?.id !== desiredNetworkId && isConnected ? (
           <Button variant="solid" color="danger" onClick={handleSwitchChain}>Switch to Mainnet</Button>
@@ -111,15 +109,38 @@ export default function Home() {
       </div>
       <div className="text-center mt-4">
         {isConnected ? (
-          hasClaimed ? (
-            <p>You&apos;ve already claimed your airdrop!</p>
-          ) : (
-            isEligible ? (
-              <p>Congrats! You&apos;re eligible to claim {userData.amount} $btc</p>
-            ) : (
-              <p>You&apos;re not eligible for an airdrop</p>
-            )
-          )
+          <>
+            <div className="mt-1 flex flex-col items-center">
+              <Input
+                value={manualAddress}
+                onChange={(e) => setManualAddress(e.target.value)}
+                placeholder="Enter address"
+                className="mb-2 w-[22rem]"
+                variant={'bordered'}
+                label="Receiver Address:"
+              />
+              {manualCheck && (
+                hasClaimed ? (
+                  <p>You&apos;ve already claimed your airdrop!</p>
+                ) : (
+                  isEligible ? (
+                    <p className="text-green-800">Congrats! You&apos;re eligible to claim {userData.amount} $btc</p>
+                  ) : (
+                    <p className="text-red-800">You&apos;re not eligible for an airdrop</p>
+                  )
+                )
+              )}
+              <Button className='mt-2' variant="solid" onClick={handleManualCheck}>Check Eligibility</Button>
+            </div>
+            <Button
+              variant="solid"
+              isDisabled={!isEligible || hasClaimed}
+              onClick={() => claimAirdrop(simulateClaimAirdrop?.request)}
+              className="mt-2 mb-4"
+            >
+              Claim
+            </Button>
+          </>
         ) : (
           <>
             <p>Please connect your wallet or manually check an address:</p>
@@ -128,32 +149,23 @@ export default function Home() {
                 value={manualAddress}
                 onChange={(e) => setManualAddress(e.target.value)}
                 placeholder="Enter address"
-                className="mb-4"
+                className="mb-2 w-[22rem]"
                 variant={'bordered'}
               />
-              <Button variant="solid" onClick={handleManualCheck}>Check Eligibility</Button>
+              {manualCheck && !isConnected && (
+                <div className="text-center">
+                  {isEligible ? (
+                    <p className="text-green-800">Congrats! The entered address is eligible to claim {userData.amount} $btc</p>
+                  ) : (
+                    <p className="text-red-800">The entered address is not eligible for an airdrop</p>
+                  )}
+                </div>
+              )}
+              <Button className='mt-2 mb-4' variant="solid" onClick={handleManualCheck}>Check Eligibility</Button>
             </div>
           </>
         )}
       </div>
-      {manualCheck && !isConnected && (
-        <div className="text-center mt-4">
-          {isEligible ? (
-            <p className="text-green-800">Congrats! The entered address is eligible to claim {userData.amount} $btc</p>
-          ) : (
-            <p className="text-red-800">The entered address is not eligible for an airdrop</p>
-          )}
-        </div>
-      )}
-      {isConnected && <Button
-        variant="solid"
-        isDisabled={!isEligible || hasClaimed}
-        onClick={() => claimAirdrop(simulateClaimAirdrop?.request)}
-        className="mt-4"
-      >
-        Claim
-      </Button>}
-
       <div className='flex flex-row bg-bitcoinorange gap-5 p-3 pl-7 pr-7 rounded-xl mt-4'>
         <Link href={`https://etherscan.io/address/0xbA51047a91d5bF6b86777A594D1714E8a337D120`} isExternal>
           <Image
@@ -185,7 +197,7 @@ export default function Home() {
           /></Link>
       </div>
       <div className='flex flex-row mt-1'>
-      <p>made by&nbsp;</p> <Link href={`https://twitter.com/tschoerv`} isExternal>tschoerv.eth</Link> <p>&nbsp;- donations welcome!</p>
+        <p>made by&nbsp;</p> <Link href={`https://twitter.com/tschoerv`} isExternal>tschoerv.eth</Link> <p>&nbsp;- donations welcome!</p>
       </div>
     </main>
   );
--- app/providers.js
+++ app/providers.js
@@ -33,9 +33,7 @@ export function Providers({ children }) {
       <QueryClientProvider client={client}>
         <RainbowKitProvider>
         <NextUIProvider>
-        <main className="text-foreground bg-background">
         {mounted && children}
-        </main>
         </NextUIProvider>
         </RainbowKitProvider>
         </QueryClientProvider>
